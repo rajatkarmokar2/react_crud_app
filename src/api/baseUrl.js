266df33d6a0f8c6293b43f7fdc747534ec75be15
rajatkarmokar2 <rajatkarmokar2@gmail.com>
2024-10-13 19:06:00 +0530

    updated packages

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const baseurl = axios.create( {
     baseURL: 'http://localhost:5000/api/v1/'
+    // baseURL: 'https://mongo-node-crud-api.netlify.app/.netlify/functions/api/' // wrong api
 } )
 
 export default baseurl
